2ac827eb917069c0007479807cb5ab10041dc212
htpusa <taneli.pusa@pm.me>
2022-07-01 14:14:14 +0300

    update tutorial

--- tutorials/tutorial_scca_hsic.m
+++ tutorials/tutorial_scca_hsic.m
@@ -2,11 +2,11 @@
 clear
 
 % generate data
-n = 500;
+n = 1000;
 p = 20;
 q = 20;
 numx = 2;
-type = 1;
+type = 5;
 [X,Y] = generate_data(n,p,q,numx,type);
 
 % standardise
@@ -33,12 +33,21 @@ hyperparams.maxit = 500;
 hyperparams.flag = 2;
 
 % train an scca-hsic model
-[u,v,hsic_train,tempobj,InterMediate] = scca_hsic(Xtrain,Ytrain,hyperparams);
+[u,v] = scca_hsic(Xtrain,Ytrain,hyperparams);
 
 % test the scca-hsic model 
 Kxtest = rbf_kernel(Xtest * u);
 Kytest = centre_kernel(rbf_kernel(Ytest * v));
 hsic_test = f(Kxtest,Kytest);
+
+% visualise
+figure
+    subplot(1,2,1)
+    scatter(sum(X(:,1:2),2), sum(Y(:,1:2),2))
+    title('true relation')
+    subplot(1,2,2)
+    scatter(Xtest*u,Ytest*v)
+    title('inferred relation')
 
 
 
